306fc321e117c98168b306ee91994a58a1f88929
Caroline <caroline.portilla@gmail.com>
2020-08-26 13:19:15 +0800

    admin and login page linked in

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 import LandingPage from "views/LandingPage/LandingPage.js";
+import AdminPage from "views/AdminPage/AdminPage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
 
 var hist = createBrowserHistory();
 
@@ -11,6 +13,8 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/landing-page" component={LandingPage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/admin-page" component={AdminPage} />
       <Route path="/" component={LandingPage} />
     </Switch>
   </Router>,
